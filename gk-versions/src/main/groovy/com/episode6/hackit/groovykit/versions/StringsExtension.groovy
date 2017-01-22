35fe2c970fbddda645f0e926ccc485b2c993936a
Geoff Hackett <ghackett@gmail.com>
2017-01-22 11:22:32 -0500

    initial project structure for gk-versions

---
+++ gk-versions/src/main/groovy/com/episode6/hackit/groovykit/versions/StringsExtension.groovy
@@ -0,0 +1,9 @@
+package com.episode6.hackit.groovykit.versions
+
+/**
+ * Extension methods Strings relating to version-compares
+ */
+class StringsExtension {
+
+  
+}
---
+++ gk-versions/src/test/groovy/com/episode6/hackit/groovykit/versions/StringsExtensionTest.groovy
@@ -0,0 +1,17 @@
+package com.episode6.hackit.groovykit.versions
+
+import spock.lang.Specification
+
+/**
+ * Tests methods added by {@link StringsExtension}
+ */
+class StringsExtensionTest extends Specification {
+
+  def "placeholder test"() {
+    when:
+    def x = true
+
+    then:
+    x
+  }
+}
